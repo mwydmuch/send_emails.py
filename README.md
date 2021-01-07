b341b4dcddf3cfdb37c7b671534cae25d0b5cb1f
Marek Wydmuch <marek@wydmuch.poznan.pl>
2021-01-07 16:42:32 +0100

    Update send_emails.py + README.md

--- README.md
+++ README.md
@@ -7,25 +7,25 @@
 Usage: send_emails.py [OPTIONS]
 
 Options:
-  -c, --content_path TEXT       Path to YAML or JSON file with email content.
+  -t, --template_path PATH      Path to YAML or JSON file with email template.
                                 The file should contain fields 'subject',
                                 'from', 'html' and 'plain'.  [required]
 
-  -a, --address_book_path TEXT  Path to TSV or CSV file with emails and
-                                additional data. File should contain field
-                                'email'.  [required]
+  -a, --address_book_path PATH  Path to TSV or CSV file with emails and
+                                additional data for the template. File should
+                                contain field 'email'.  [required]
 
-  -d, --delimiter TEXT          Delimiter for address book file (default =
-                                '\t')
+  -d, --delimiter TEXT          Delimiter for address book file.
+                                [default: '\t']
 
   -h, --host TEXT               SMTP host.  [required]
   -l, --login TEXT              SMTP login.  [required]
   -p, --password TEXT           SMTP password.  [required]
-  -P, --port INTEGER            SMTP port (default = 587).
+  -P, --port INTEGER            SMTP port.  [default: 587]
   -e, --email TEXT              Send all messages to the specified email.
-  -b, --batchsize INTEGER       Size of email batches (default 10)
-  -s, --sleeptime INTEGER       Sleep interval between sending email batches
-                                (default 0)
+  -b, --batchsize INTEGER       Size of email batches.  [default: 10]
+  -s, --sleeptime INTEGER       Sleep interval between sending email batches.
+                                [default: 0]
 
   --help                        Show this message and exit.
 ```
--- send_emails.py
+++ send_emails.py
@@ -3,7 +3,7 @@
 
 """
 send_emails.py
-Copyright (c) 2019-2020 Marek Wydmuch
+Copyright (c) 2019-2021 Marek Wydmuch
 """
 
 import click
@@ -29,17 +29,17 @@ def send_mail(server, msg):
     print("Successfully sent email to:", msg["To"])
 
 
-def compose_msg(address, content):
+def compose_msg(template, address):
     msg = MIMEMultipart("alternative")
 
-    msg["From"] = content["from"]
+    msg["From"] = template["from"]
     msg["To"] = address["email"]
-    msg["Subject"] = content["subject"].format(**address)
+    msg["Subject"] = template["subject"].format(**address)
 
-    msg.add_header("Content-Type", "text/html")
+    msg.add_header("template-Type", "text/html")
 
-    part1 = MIMEText(content["plain"].format(**address), "plain")
-    part2 = MIMEText(content["html"].format(**address), "html")
+    part1 = MIMEText(template["plain"].format(**address), "plain")
+    part2 = MIMEText(template["html"].format(**address), "html")
     msg.attach(part1)
     msg.attach(part2)
 
@@ -48,30 +48,30 @@ def compose_msg(address, content):
 
 @click.command()
 @click.option(
-    "-c",
-    "--content_path",
-    type=str,
+    "-t",
+    "--template_path",
+    type=click.Path(exists=True),
     required=True,
-    help="Path to YAML or JSON file with email content. The file should contain fields 'subject', 'from', 'html' and 'plain'.",
+    help="Path to YAML or JSON file with email template. The file should contain fields 'subject', 'from', 'html' and 'plain'.",
 )
 @click.option(
     "-a",
     "--address_book_path",
-    type=str,
+    type=click.Path(exists=True),
     required=True,
-    help="Path to TSV or CSV file with emails and additional data. File should contain field 'email'.",
+    help="Path to TSV or CSV file with emails and additional data for the template. File should contain field 'email'.",
 )
 @click.option(
     "-d",
     "--delimiter",
     type=str,
     default="\t",
-    help="Delimiter for address book file (default = '\\t') ",
+    help="Delimiter for address book file.\t[default: '\\t']",
 )
 @click.option("-h", "--host", type=str, required=True, help="SMTP host.")
 @click.option("-l", "--login", type=str, required=True, help="SMTP login.")
 @click.option("-p", "--password", type=str, required=True, help="SMTP password.")
-@click.option("-P", "--port", type=int, default=587, help="SMTP port (default = 587).")
+@click.option("-P", "--port", type=int, default=587, help="SMTP port.", show_default=True)
 @click.option(
     "-e",
     "--email",
@@ -79,25 +79,25 @@ def compose_msg(address, content):
     default=None,
     help="Send all messages to the specified email.",
 )
-@click.option("-b", "--batchsize", type=int, default=10, help="Size of email batches (default 10)")
-@click.option("-s", "--sleeptime", type=int, default=0, help="Sleep interval between sending email batches (default 0)")
+@click.option("-b", "--batchsize", type=int, default=10, help="Size of email batches.", show_default=True)
+@click.option("-s", "--sleeptime", type=int, default=0, help="Sleep interval between sending email batches.", show_default=True)
 def send_mails(
     host: str,
     login: str,
     password: str,
     port: int,
-    content_path: str,
+    template_path: str,
     address_book_path: str,
     delimiter: str,
     email: str,
     batchsize: int,
     sleeptime: int,
 ):
-    with open(content_path, "r", encoding="utf-8") as content_file:
-        if content_path.endswith(".yaml"):
-            content = yaml.safe_load(content_file)
-        elif content_path.endswith(".json"):
-            content = json.loads(content_file)
+    with open(template_path, "r", encoding="utf-8") as template_file:
+        if template_path.endswith(".yaml"):
+            template = yaml.safe_load(template_file)
+        elif template_path.endswith(".json"):
+            template = json.loads(template_file)
 
     with open(address_book_path, "r", encoding="utf-8") as addressees_file:
         addressees_reader = csv.DictReader(addressees_file, delimiter=delimiter)
@@ -105,7 +105,7 @@ def send_mails(
         for i, address in enumerate(addressees_reader):
             if email and len(email) > 0:
                 address["email"] = email
-            send_mail(server, compose_msg(address, content))
+            send_mail(server, compose_msg(template, address))
 
             if sleeptime > 0 and i > 0 and i % batchsize == 0:
                 print("Sent {} messages, waiting {} seconds ...".format(batchsize, sleeptime))
